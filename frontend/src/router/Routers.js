5e89664f22ab7c0123abb44be0b9862fbf33943e
Truong Minh Huu <minhhuu0705@gmail.com>
2024-09-13 23:37:33 +0700

    first commit

---
+++ frontend/src/router/Routers.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Routes, Route} from "react-router-dom"
+import Home from "../pages/Home";
+import Login  from '../pages/Login';
+import Register from '../pages/Register';
+import SearchResultList from '../pages/SearchResultList';
+import TourDetails from '../pages/TourDetails';
+import Tours from '../pages/Tours';
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigate to='/home'/>}/>
+      <Route path='/home' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/tours/search' element={<SearchResultList/>}/>
+      <Route path='/tours' element={<Tours/>}/>
+      <Route path='/tours/:id' element={<TourDetails/>}/>
+    </Routes>
+  )
+}
+
+export default Router
