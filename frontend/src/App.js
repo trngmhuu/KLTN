54b06abbb3f6702b0ee29620aa314f962c645a25
unknown <minhhuu0705@gmail.com>
2024-12-06 12:19:00 +0700

    fix browser router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import Layout from "./components/Layout/Layout";
 import { NotificationProvider } from "./context/NotificationContext"
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
